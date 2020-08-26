aa8a9e4d590d223b386eff6c3927fd61dd712e21
NeoNexus DeMortis <neonexus.demortis@gmail.com>
2020-08-26 15:50:11 -0500

    Removed need for use of EJS when serving React apps.

--- assets/src/Main/Main.jsx
+++ assets/src/Main/Main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Button, Container} from 'react-bootstrap';
+import img from '../../images/sails-logo.png';
 
 function Main() {
     return (
         <Container id="main-wrapper" style={{paddingTop: '50px'}}>
+            <div className="text-center">
+                <img src={img} alt="Sails Logo" />
+            </div>
             <h1>Main "application"</h1>
             <div>The marketing site, if you will.</div>
             <br />
